d75a92b3402c188737e0ab1f5a41a2f8cabdcffa
nurullah <nurullah7733@gmail.com>
2022-02-17 15:35:07 +0600

    magicReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,52 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useReducer, useState } from "react";
+
+function reducer({ state }) {
+  // eslint-disable-next-line default-case
+  switch (state) {
+    case "pressed_once":
+      return {
+        state: "pressed_two",
+      };
+    case "pressed_two":
+      return {
+        state: "pressed_three",
+      };
+    case "pressed_three":
+      return {
+        state: "pressed_once",
+      };
+  }
+}
+
+const Button = (props) => {
+  const [state, dispatch] = useReducer(reducer, { state: "pressed_once" });
+  return (
+    <div
+      style={{
+        color: props.color,
+        textDecoration: props.underline ? "underline" : undefined,
+      }}
+      onClick={() => dispatch()}
+    >
+      Click Me {state.state}
+    </div>
+  );
+};
 
 function App() {
-  return <div className="App">bismillah</div>;
+  const props = {
+    increment: 2,
+    underline: true,
+    color: "black",
+  };
+  return (
+    <div className="App">
+      <Button color="red" />
+      <Button color="green" />
+      <Button color="blue" />
+    </div>
+  );
 }
 
 export default App;
